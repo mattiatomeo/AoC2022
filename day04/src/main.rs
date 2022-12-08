d38321bdba64ce8a75a3d51c12fae81b19cf26a9
Mattia Tomeo <tomeo.mat@gmail.com>
2022-12-08 20:22:12 +0100

    Type aliases and rename function in day 4

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -1,8 +1,12 @@
 use std::fs;
 
+type ElfSections = (u32, u32);
+type InputRow = (ElfSections, ElfSections);
+type InputType = Vec<InputRow>;
 
-fn read_input(filename: &str) -> Vec<((u32, u32), (u32, u32))> {
-    fn build_section_range(section_range: &str) -> (u32, u32) {
+
+fn read_input(filename: &str) -> InputType {
+    fn build_section_range(section_range: &str) -> ElfSections {
         let section_extremes: Vec<u32> = section_range.split('-')
             .map(|section| section.parse::<u32>().unwrap())
             .collect();
@@ -10,7 +14,7 @@ fn read_input(filename: &str) -> Vec<((u32, u32), (u32, u32))> {
         (section_extremes[0], section_extremes[1])
     }
 
-    fn build_pair_sections(pair_section: &str) -> ((u32, u32), (u32, u32)) {
+    fn build_pair_sections(pair_section: &str) -> InputRow {
         let mut parsed_sections = pair_section.split(',')
             .map(build_section_range);
 
@@ -28,21 +32,21 @@ fn read_input(filename: &str) -> Vec<((u32, u32), (u32, u32))> {
         .collect()
 }
 
-fn is_first_section_in_second(first_sections: &(u32, u32), second_sections: &(u32, u32)) -> bool {
+fn is_a_section_in_the_other(first_sections: &ElfSections, second_sections: &ElfSections) -> bool {
     let first_in_second = (first_sections.0 >= second_sections.0) && (first_sections.1 <= second_sections.1);
     let second_in_first = (first_sections.0 <= second_sections.0) && (first_sections.1 >= second_sections.1);
 
     first_in_second || second_in_first
 }
 
-fn step_1(elf_sections_pairs: &Vec<((u32, u32),(u32, u32))>) -> u32 {
+fn step_1(elf_sections_pairs: &InputType) -> u32 {
     elf_sections_pairs.iter()
-        .map(|sections_pair| is_first_section_in_second(&sections_pair.0, &sections_pair.1))
+        .map(|sections_pair| is_a_section_in_the_other(&sections_pair.0, &sections_pair.1))
         .filter(|is_first_in_second| *is_first_in_second)
         .count() as u32
 }
 
-fn are_sections_overlap(first_sections: &(u32, u32), second_sections: &(u32, u32)) -> bool {
+fn are_sections_overlap(first_sections: &ElfSections, second_sections: &ElfSections) -> bool {
     let is_first_extreme_in_second = first_sections.0 >= second_sections.0 && first_sections.0 <= second_sections.1;
     let is_second_extreme_in_second = first_sections.1 >= second_sections.0 && first_sections.1 <= second_sections.1;
 
@@ -52,7 +56,7 @@ fn are_sections_overlap(first_sections: &(u32, u32), second_sections: &(u32, u32
     is_first_extreme_in_second || is_second_extreme_in_second || is_first_extreme_in_first || is_second_extreme_in_first
 }
 
-fn step_2(elf_sections_pairs: &Vec<((u32, u32),(u32, u32))>) -> u32 {
+fn step_2(elf_sections_pairs: &InputType) -> u32 {
     elf_sections_pairs.iter()
         .map(|sections_pair| are_sections_overlap(&sections_pair.0, &sections_pair.1))
         .filter(|is_first_in_second| *is_first_in_second)
